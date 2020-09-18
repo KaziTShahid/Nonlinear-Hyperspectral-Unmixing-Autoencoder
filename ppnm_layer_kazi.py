3eecd835e57db93fdeb6bebf0bf56cedd3d3a133
Kazi T Shahid <67612009+KaziTShahid@users.noreply.github.com>
2020-09-17 22:28:13 -0500

    Update ppnm_layer_kazi.py

--- ppnm_layer_kazi.py
+++ ppnm_layer_kazi.py
@@ -2,7 +2,23 @@
 """
 Created on Fri Jul 10 20:48:35 2020
 
-@author: Shounak
+@author: Kazi T Shahid and Ioannis D Schizas
+
+This code is used for the script "autoencoder_main.py"
+
+This is the Final Layer, where the input is the vector containing abundances, and their cross products.
+
+Input:
+initial_endmembers = weights that are initialized with the endmember matrices. These weights will determine the final estimated values of the endmembers.
+
+Weights:
+linear_endmembers = named so, because they make up the endmembers in the linear component of the mixture, and their cross-products are later used to 
+estimate the nonlinear components of the mixture model.
+b_s = what value is used to control the contribution of the nonlinear components of the mixture model, w.r.t. to the entire mixture. This determines how
+strong the nonlinear component is, compared to the linear component.
+
+If you wish to use this code, please cite the URL given above for the dataset, and also the URL where this code was downloaded from:
+https://github.com/KaziTShahid/Nonlinear-Hyperspectral-Unmixing-Autoencoder
 """
 
 import tensorflow as tf
